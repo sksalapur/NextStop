e4ce22a250b2dc4ce9bbc7e579162fa5701f786d
sksalapur <sksalapur@example.com>
2026-04-16 00:43:28 +0530

    chore: bump version to 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 2
-        versionName = "2.0"
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
